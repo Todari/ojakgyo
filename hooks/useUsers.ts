97d9131946bf963455a632a4efe4b7a85c7d2007
이태훈 <rhymint@gmail.com>
2025-06-25 07:39:00 +0900

    feat: useUser hook 구현

---
+++ hooks/useUsers.ts
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react';
+import { supabase } from '@/utils/supabase';
+import { User } from '@/types/model';
+import { Alert } from 'react-native';
+
+export function useUsers() {
+  const [users, setUsers] = useState<User[] | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState<Error | null>(null);
+
+  useEffect(() => {
+    async function fetchUsers() {
+      setIsLoading(true);
+      setError(null);
+      const { data, error } = await supabase
+        .from('user')
+        .select("id, name, lat, lng")
+        // .select('id, name, lat, lng')
+        // .order('id', { ascending: true });
+      console.log(data, error)
+      if (error) {
+        Alert.alert('error', error.message);
+        setError(error);
+        setUsers(null);
+      } else if (data) {
+        setUsers(
+          data.map(user => ({
+            id: user.id,
+            name: user.name,
+            lat: Number(user.lat),
+            lng: Number(user.lng),
+          }))
+        );
+      }
+      setIsLoading(false);
+    }
+    fetchUsers();
+  }, []);
+
+  return { users, isLoading, error };
+}
