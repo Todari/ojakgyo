86100ac9c7138bb6226bd3a56b1ba22dab8052ea
이태훈 <rhymint@gmail.com>
2025-08-22 10:02:44 +0900

    design: tab bar design 변경

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,34 +1,54 @@
 import React from 'react';
 import { Tabs } from 'expo-router';
 import { Ionicons } from '@expo/vector-icons';
+import Colors from '@/constants/colors';
+import { View } from 'react-native';
 
 export default function TabsLayout() {
   return (
-    <Tabs screenOptions={{ headerShown: false }}>
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: Colors.light.tint,
+        tabBarInactiveTintColor: '#9CA3AF',
+        tabBarStyle: {
+          height: 88,
+          paddingTop: 6,
+          paddingBottom: 18,
+          backgroundColor: 'white',
+          borderTopWidth: 0.5,
+          borderTopColor: '#E5E7EB',
+        },
+      }}
+    >
       <Tabs.Screen
         name="helper/index"
         options={{
-          title: '홈',
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="home" size={size ?? 22} color={color} />
+          tabBarIcon: ({ color }) => (
+            <View style={{ transform: [{ translateY: 6 }] }}>
+              <Ionicons name="home" size={32} color={color} />
+            </View>
           ),
         }}
       />
       <Tabs.Screen
         name="chat/index"
         options={{
-          title: '채팅',
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="chatbubbles" size={size ?? 22} color={color} />
+          tabBarIcon: ({ color }) => (
+            <View style={{ transform: [{ translateY: 6 }] }}>
+              <Ionicons name="chatbubbles" size={32} color={color} />
+            </View>
           ),
         }}
       />
       <Tabs.Screen
         name="profile/index"
         options={{
-          title: '프로필',
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="person" size={size ?? 22} color={color} />
+          tabBarIcon: ({ color }) => (
+            <View style={{ transform: [{ translateY: 6 }] }}>
+              <Ionicons name="person" size={32} color={color} />
+            </View>
           ),
         }}
       />
